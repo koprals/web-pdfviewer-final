9a6140c31754c11e08dea298a29c260ac111f7b7
Jumraty Ahmad <jumratyahmad@MacBook-Air-Jumraty.local>
2025-08-03 11:11:30 +0700

    final

--- db.php
+++ db.php
@@ -1,20 +1,19 @@
 <?php
-$host = 'z8vpra.h.filess.io';
-$port = 61000;
-$db   = 'epas_oncechoice';
-$user = 'epas_oncechoice';
-$pass = 'ec9af42aafc79b74c2571ddfe9f06921b56758c5';
-$charset = 'utf8mb4';
 
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$db = getenv('DB_DATABASE');
+$user = getenv('DB_USERNAME');
+$pass = getenv('DB_PASSWORD');
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+    exit('Database connection failed: '.$e->getMessage());
 }
