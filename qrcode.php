824ba8bea4eee6b956ddc00cfc4bd7ab66a91780
Jumraty Ahmad <jumratyahmad@MacBook-Air-Jumraty.local>
2025-08-04 12:28:19 +0700

    Add homepage

--- qrcode.php
+++ qrcode.php
@@ -1,17 +1,35 @@
 <?php
+
 require_once 'vendor/autoload.php';
+
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\Logo\Logo;
 use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Writer\PngWriter;
 
-if (!isset($_GET['text'])) {
+if (! isset($_GET['text'])) {
     http_response_code(400);
     exit('Missing text parameter.');
 }
 
 $text = $_GET['text'];
-$qr = QrCode::create($text)->setSize(150)->setMargin(10);
-$writer = new PngWriter();
-$result = $writer->write($qr);
+
+// Buat QR Code
+$qr = QrCode::create($text)
+    ->setSize(300)
+    ->setMargin(10)
+    ->setErrorCorrectionLevel(new ErrorCorrectionLevelHigh); // Gunakan tingkat koreksi tinggi
+
+$writer = new PngWriter;
+
+// Tambahkan logo
+$logoPath = __DIR__.'/logo.png';
+if (file_exists($logoPath)) {
+    $logo = Logo::create($logoPath)->setResizeToWidth(60); // Sesuaikan ukuran logo
+    $result = $writer->write($qr, null, $logo);
+} else {
+    $result = $writer->write($qr);
+}
 
 header('Content-Type: image/png');
 echo $result->getString();
